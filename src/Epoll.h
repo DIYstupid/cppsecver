9a73429ba9244cdd4cbecc38e0701a4f2c038861
DIYstupid <1228505042@qq.com>
2025-06-09 20:13:36 +0800

    封装了client,增加了channel类获取更多event信息

--- src/Epoll.h
+++ src/Epoll.h
@@ -7,11 +7,13 @@ class Epoll
 private:
     int epfd;
     struct epoll_event *events;
+
 public:
     Epoll();
     ~Epoll();
 
     void addFd(int fd, uint32_t op);
-    std::vector<epoll_event> poll(int timeout = -1);
+    // std::vector<epoll_event> poll(int timeout = -1);
+    std::vector<Channel *> poll(int timeout = -1);
+    void updateChannel(Channel *);
 };
-
